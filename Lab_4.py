e7a41978abfbf2db38f18d0d61e0604067fc69ee
Антон <vllasenkoaa@gmail.com>
2025-12-07 16:33:20 +0300

    Third commit

--- Lab_4.py
+++ Lab_4.py
@@ -1,23 +1,23 @@
 # Задание 1: Защита данных пользователя
-# class UserAccount:
-#     def __init__(self, username, email, password):
-#         self.username = username
-#         self.email = email
-#         self.__password = password
-#     def set_password(self, new_password):
-#         if len(new_password) >= 8:
-#             self.__password = new_password
-#             print("Пароль успешно изменен")
-#         else:
-#             print("Ошибка: пароль слишком короткий")
-#     def check_password(self, password):
-#             return password == self.__password
-# user = UserAccount("Anton", "Anton@yandex.ru", "qwerty123")
-# print(user.check_password("12345"))
-# print(user.check_password("qwerty123"))
-#
-# user.set_password("new_password")
-# print(user.check_password("new_password"))
+class UserAccount:
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.__password = password
+    def set_password(self, new_password):
+        if len(new_password) >= 8:
+            self.__password = new_password
+            print("Пароль успешно изменен")
+        else:
+            print("Ошибка: пароль слишком короткий")
+    def check_password(self, password):
+            return password == self.__password
+user = UserAccount("Anton", "Anton@yandex.ru", "qwerty123")
+print(user.check_password("12345"))
+print(user.check_password("qwerty123"))
+
+user.set_password("new_password")
+print(user.check_password("new_password"))
 
 # Задание 2: Полиморфизм и наследование
 # class Vehicle:
--- Lab_5.py
+++ Lab_5.py
@@ -1,1 +1,59 @@
-#1231412
+class Employee:
+    def __init__(self, name, id):
+        self.name = name
+        self.id = id
+    def get_info(self):
+        return "Имя сотрудника: {}, Идентификационный номер: {}".format(self.name, self.id)
+class Manager(Employee):
+    def __init__(self, name, id, department):
+        # super().__init__(name, id)
+        Employee.__init__(self, name, id)
+        self.department = department
+    def get_info(self):
+        return "Имя сотрудника: {}, Идентификационный номер: {}".format(self.name, self.id)
+    def manage_project(self):
+        return "Управляет проектами: {}".format(self.department)
+class Technician(Employee):
+    def __init__(self, name, id, specialization):
+        super().__init__(name, id)
+        self.specialization = specialization
+    def get_info(self):
+        return "Техник: {}, Идентификационный номер: {}".format(self.name, self.id)
+    def perform_maintenance(self):
+        return "Выполняет техническое обслуживание: {}".format(self.specialization)
+class TechManager(Manager, Technician):
+    def __init__(self, name, id, department, specialization):
+        Manager.__init__(self, name, id, department)
+        Technician.__init__(self, name, id, specialization)
+        self.team = []
+    def get_info(self):
+        return "Технический менеджер: {}, Идентификационный номер: {}".format(self.name, self.id)
+    def tech_lead(self):
+        return "Управляет проектами: {}, Выполняет техническое обслуживание: {}".format(self.department, self.specialization)
+    def add_employee(self, employee):
+        self.team.append(employee)
+        return "Сотрудник {} добавлен в список подчиненных".format(employee.name)
+    def get_team_employee(self):
+        if not self.team:
+            return "Нет подчиненных сотрудников"
+        team_info = "Команда сотрудников:\n"
+        for employee in self.team:
+            team_info += "- " + employee.get_info() + "\n"
+        return team_info
+
+emp1 = Employee("Иван Иванов", "У1")
+manager = Manager("Петр Петров", "У2", "IT")
+tech = Technician("Сидоров", "У3", "Сети")
+tech_manager = TechManager("Маша", "У4", "Разработка", "Python")
+
+print(emp1.get_info())
+print(manager.get_info())
+print(manager.manage_project())
+print(tech.get_info())
+print(tech.perform_maintenance())
+print(tech_manager.get_info())
+print(tech_manager.tech_lead())
+
+print(tech_manager.add_employee(emp1))
+print(tech_manager.add_employee(tech))
+print(tech_manager.get_team_employee())
